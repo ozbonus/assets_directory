76523644def510fd5b2975e27d42f70b709c9468
Christopher Perry <ozbonus@gmail.com>
2024-12-24 11:58:02 +0800

    Refactor verify ffmpeg test.

--- kantan_assets/test_script.py
+++ kantan_assets/test_script.py
@@ -2,10 +2,18 @@ import pytest
 from kantan_assets import verify_ffmpeg
 
 class TestVerifyFfmpeg:
+    """
+    In order for these tests to run correctly, you should actually have ffmpeg
+    installed and compiled with libfdk_aac.
+    """
+
     def test_ffmpeg_not_installed(self):
         with pytest.raises(FileNotFoundError):
             verify_ffmpeg(["not-ffmpeg"], "libfdk_aac")
     
     def test_libfdk_aac_not_found(self):
-        with pytest.raises(SystemExit):
+        with pytest.raises(ValueError):
             verify_ffmpeg(["ffmpeg", "-encoders"], "not-libfdk_aac")
+
+    def test_ffmpeg_and_libfdk_aac_ok(self):
+        assert verify_ffmpeg()
