51b18a75ff70d8d2fc0eaf8c1f394d042ad182d8
Christopher Perry <ozbonus@gmail.com>
2024-12-26 12:23:06 +0800

    Write test for file not found.

--- kantan_assets/test_script.py
+++ kantan_assets/test_script.py
@@ -1,5 +1,5 @@
 import pytest
-from kantan_assets import verify_ffmpeg, verify_cover, verify_art
+from kantan_assets import verify_ffmpeg, verify_cover, verify_art, verify_audio
 
 
 class TestVerifyFfmpeg:
@@ -48,3 +48,15 @@ class TestVerifyArt:
     def test_art_is_not_square(self):
         with pytest.raises(ValueError, match="not square"):
             verify_art("./test_assets/art_not_square.jpg")
+
+class TestVerifyAudio:
+    def test_audio_is_good(self):
+        assert verify_audio("./test_assets/audio_good.mp3") is None
+
+    def test_audio_no_tags(self):
+        with pytest.raises(ValueError):
+            verify_audio("./test_assets/audio_no_tags.mp3")
+    
+    def test_audio_not_found(self):
+        with pytest.raises(FileNotFoundError):
+            verify_audio("./test_assets/not_a_real_file.mp3")
